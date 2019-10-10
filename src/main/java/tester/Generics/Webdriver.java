867510c0b24f9495a732b9b16bb07a847e81f5b7
Naga911 <47557222+Naga911@users.noreply.github.com>
2019-10-10 14:45:38 +0530

    Create Webdriver.java

---
+++ src/main/java/tester/Generics/Webdriver.java
@@ -0,0 +1,16 @@
+
+package tester.Generics;
+
+
+public interface Webdriver {
+
+
+    void openBrowser();
+    void closeBrowser();
+}
+
+
+   /* public DriverManager(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(new DefaultElementLocatorFactory(driver), this);
+    }*/
